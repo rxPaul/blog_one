1944ce8c8cfb2b345ed9a89384f4f76d58fc292c
rxPaul <98978708+rxPaul@users.noreply.github.com>
2022-07-05 12:41:49 +0300

    Fix Tag Form

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.posts_list, name='posts_list_url'),
-    path('post/<str:slug>/', views.PostDetail.as_view, name='post_detail_url'),
+    path('post/<str:slug>/', views.PostDetail.as_view(), name='post_detail_url'),
     path('tags/', views.tags_list, name='tags_list_url'),
-    path('tag/create', views.TagCreate.as_view, name='tag_create_url'),
-    path('tag/<str:slug>/', views.TagDetail.as_view, name='tag_detail_url')
+    path('tag/create/', views.TagCreate.as_view(), name='tag_create_url'),
+    path('tag/<str:slug>/', views.TagDetail.as_view(), name='tag_detail_url')
     ]
