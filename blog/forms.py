8f25bcf6999936b3ef02b5a13a49fcd41beb6544
rxPaul <98978708+rxPaul@users.noreply.github.com>
2022-07-05 13:42:12 +0300

    Updated tag create form

--- blog/forms.py
+++ blog/forms.py
@@ -7,11 +7,17 @@ class TagForm(forms.Form):
     title = forms.CharField(max_length=50)
     slug = forms.CharField(max_length=50)
 
+    title.widget.attrs.update({'class': 'form-control'})
+    slug.widget.attrs.update({'class': 'form-control'})
+
+
     def clean_slug(self):
         new_slug = self.cleaned_data['slug'].lower()
 
         if new_slug == 'create':
             raise ValidationError('Slug may no t be "create"')
+        if Tag.objects.filter(slug_iexact=new_slug).count():
+            raise ValidationError('Slug must be unique. We have "{}" slug already'.format(new_slug))
         return new_slug
 
     def save(self):
