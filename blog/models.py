9b7a190b1a77f79ad262a81aa15d714084db23a3
rxPaul <98978708+rxPaul@users.noreply.github.com>
2022-07-06 15:26:21 +0300

    Added get_update_url. Added function POST for TagUpdateForm

--- blog/models.py
+++ blog/models.py
@@ -34,5 +34,8 @@ class Tag(models.Model):
     def get_absolute_url(self):
         return reverse('tag_detail_url', kwargs={'slug': self.slug})
 
+    def get_update_url(self):
+        return reverse('tag_update_url', kwargs={'slug': self.slug})
+
     def __str__(self):
         return '{}'.format(self.title)
